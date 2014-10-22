38f5f15bdc07e5aa228c49737f04e5c6e18b9182
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2014-10-22 09:36:53 +0900

    recover

---
+++ main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Omothello {
+ 
+  public static final int NUM =  12;
+
+  public static void main( String[] args ) {
+    // 変数宣言
+    int turn = 1;
+    int[][] board =  {
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    };
+    int[][] val_board = new int[NUM][NUM] ;
+    Random rand = new Random();
+
+    // 点数盤面の生成
+    for ( int i = 0; i < NUM; i++ ) {
+      for ( int j = 0; j < NUM; j++ ) {
+        val_board[i][j] = (rand.nextInt(100) - 50);
+      } 
+    }
+    
+    output_bd( val_board);
+  
+  }
+
+  static void output_bd( int[][] board ) {
+    for ( int i = 0; i < NUM; i++ ) {
+      for ( int j = 0; j < NUM; j++ ) {
+        System.out.printf("%3d ", board[i][j]);
+      }
+      System.out.println();
+    }
+  }
+
+  static void process( int[][] board, int[][] val_board, int turn  ) {
+    Scanner stdIn = new Scanner(System.in); 
+
+    int x = ;
+    int y = 0; //置く場所の座標
+    
+    
+    
+    
+  }
+
+}
